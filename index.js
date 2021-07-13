d647d07d1c3bbb3de6abcbdac4af57f1f2c180ce
sergiocardenasg <sergiocardenas@utexas.edu>
2021-07-12 19:55:44 -0500

    commit tree 562810610c0439284606b28a7870956d9e0cab21

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    const regex = /[^A-Za-z0-9 '-]/g
+    return str.replace(regex, '') 
+  }
+
+  static titleize(str) {
+    const arr = str.split(" ");
+    const newArr = [];
+    let exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    for (let i = 0; i < arr.length; i++) {
+      const element = arr[i];
+      if (i == 0) {
+        newArr.push(this.capitalize(element))
+      } else {
+        if (exceptions.includes(element)) {
+          newArr.push(element)
+        } else {
+          newArr.push(this.capitalize(element))
+        }
+      }
+    }
+    return newArr.join(" ")
+  }
 }
